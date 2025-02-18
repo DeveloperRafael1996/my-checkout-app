cbf5eda007e1c0d96e36d73324fabe2ab769394b
Rafael Guevara Aller <djzm80@gmail.com>
2025-02-18 02:31:40 -0500

    feat: logic new pay

---
+++ src/app/components/checkout.niubiz.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Script from "next/script";
+import pino from "pino";
+import { DecryptUrlResponse } from "../dto/decry.dto";
+
+const logger = pino();
+const checkoutScript = process.env.NEXT_PUBLIC_CHECKOUT_SCRIPT;
+
+interface CheckoutFormProps {
+  bodyPay: DecryptUrlResponse | null;
+  sessionKey: string;
+}
+
+const CheckoutFormNiubiz: React.FC<CheckoutFormProps> = ({
+  bodyPay,
+  sessionKey,
+}) => {
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    const openForm = () => {
+      const apiUrl = `/api/payment?amount=${bodyPay?.amount}&purchaseNumber=${bodyPay?.purchaseNumber}`;
+      const logo = "http://localhost:3000/images/belity-app.png";
+
+      if (window.VisanetCheckout) {
+        window.VisanetCheckout.configure({
+          sessiontoken: sessionKey,
+          channel: "web",
+          merchantid: "456879852",
+          purchasenumber: bodyPay?.purchaseNumber,
+          amount: bodyPay?.amount,
+          expirationminutes: "20",
+          timeouturl: "/",
+          merchantlogo: logo,
+          action: apiUrl,
+          formbuttoncolor: "#3900AC",
+          buttonsize: "LARGE",
+          hidexbutton: "true",
+          usertoken: bodyPay?.clientMail,
+        });
+
+        window.VisanetCheckout.open();
+      } else {
+        logger.error("VisanetCheckout Script Not Loaded Property");
+      }
+    };
+
+    if (isScriptLoaded && sessionKey && bodyPay) {
+      openForm();
+    }
+  }, [sessionKey, isScriptLoaded, bodyPay]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={checkoutScript}
+        onLoad={() => {
+          logger.info("Checkout Script Loaded Successfully");
+          setIsScriptLoaded(true);
+        }}
+      />
+    </>
+  );
+};
+
+export default CheckoutFormNiubiz;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import CheckoutForm from "./components/checkout.form";
+"use client";
+//import CheckoutForm from "./components/checkout.form";
+import { useSearchParams } from "next/navigation";
+import { usePaymentSetup } from "./hooks/usePaymentSetup";
+import CheckoutFormNiubiz from "./components/checkout.niubiz";
 
+/*
 export default function Home() {
   return (
     <div>
@@ -7,3 +12,17 @@ export default function Home() {
     </div>
   );
 }
+
+*/
+
+const Home = () => {
+  const searchParams = useSearchParams();
+  const dataParams = searchParams.get("data");
+  const ivParams = searchParams.get("iv");
+
+  const { bodyPay, sessionKey } = usePaymentSetup(dataParams, ivParams);
+
+  return <CheckoutFormNiubiz bodyPay={bodyPay} sessionKey={sessionKey} />;
+};
+
+export default Home;
