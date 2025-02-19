9ea32991716e5ba6420e16e2f88cbccd2c97993d
Rafael Guevara Aller <rafa291996@hotmail.com>
2025-02-19 12:01:34 -0500

    feat: change url ms-service

--- src/app/http/enpoint.ts
+++ src/app/http/enpoint.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+//const API_BASE_URL_MS = "http://localhost:8080";
 const API_BASE_URL = "https://apisandbox.vnforappstest.com";
-const API_BASE_URL_MS = "http://localhost:8080";
+const API_BASE_URL_MS = "https://ms-pay-prod-700901035298.us-central1.run.app";
 
 export const http = axios.create({
   baseURL: API_BASE_URL,
