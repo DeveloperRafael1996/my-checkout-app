dee08ec6e1b3ebaaa139a96518145ccf088aa28a
Rafael Guevara Aller <djzm80@gmail.com>
2025-02-19 21:59:30 -0500

    feat: change url for nexurl

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -4,19 +4,23 @@ export async function POST(req: NextRequest) {
   try {
     const response = await req.formData();
 
-    const url = response.get("url") as string;
-    const transactionToken = response.get("transactionToken") as string;
-    const channel = response.get("channel") as string;
+    /*
+      const url = response.get("url") as string;
+      const channel = response.get("channel") as string;
+    */
 
-    const { searchParams } = new URL(req.url);
+    const transactionToken = response.get("transactionToken") as string;
+    const { searchParams } = new URL(req.nextUrl);
     const amount = searchParams.get("amount");
     const purchaseNumber = searchParams.get("purchaseNumber");
 
+    /* Pay Efectivo
     if (channel === "pagoefectivo") {
       //CIP --> TransactionToken
       //Save Code CIP BD Associate To Order
       return NextResponse.redirect(url);
     }
+      */
 
     return NextResponse.redirect(
       new URL(
