82224773f371aa52aac87508ef909f9138ca5b99
Rafael Guevara Aller <rafa291996@hotmail.com>
2025-02-21 17:21:50 -0500

    feat: add fix

--- src/app/actions/payment-configuration-setup.actiont.ts
+++ src/app/actions/payment-configuration-setup.actiont.ts
@@ -3,27 +3,30 @@ import { mapperSession } from "../mapper/session.mapper";
 import { apiSession, decryptUrl } from "./payment-setup.action";
 
 interface InitPaymentOptions {
-    data:string;
-    iv: string;
+  data: string;
+  iv: string;
 }
 
 interface InitPaymentResponse {
-    bodyPay: DecryptUrlResponse;
-    sessionKey: string;
+  bodyPay: DecryptUrlResponse;
+  sessionKey: string;
 }
 
-export const initPaymentConfiguration= async ({ data, iv }: InitPaymentOptions): Promise<InitPaymentResponse>  => {
+export const initPaymentConfiguration = async ({
+  data,
+  iv,
+}: InitPaymentOptions): Promise<InitPaymentResponse> => {
   const decryptResponse = await decryptUrl({
     data,
     iv,
   });
 
   const requestSession = mapperSession(decryptResponse);
-
-  const {sessionKey} = await apiSession(requestSession);
+  console.log(`Request `, { requestSession });
+  const { sessionKey } = await apiSession(requestSession);
 
   return {
     bodyPay: decryptResponse,
-    sessionKey
-  }
+    sessionKey,
+  };
 };
--- src/app/http/enpoint.ts
+++ src/app/http/enpoint.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
-
-//const API_BASE_URL_MS = "http://localhost:8080";
+/*
+  const API_BASE_URL_MS = "http://localhost:8080";
+*/
 const API_BASE_URL_NIUBIZ = "https://apisandbox.vnforappstest.com";
-//const API_BASE_URL_MS_PAY = process.env.NEXT_PUBLIC_BASE_URL_MS_PAY;
-
+const API_BASE_URL_MS_PAY = process.env.BASE_URL_MS_PAY;
 export const http = axios.create({
   baseURL: API_BASE_URL_NIUBIZ,
   headers: { "Content-Type": "application/json" },
 });
 
 export const http_pay = axios.create({
-  baseURL: "https://ms-pay-dev-700901035298.us-central1.run.app",
+  baseURL: API_BASE_URL_MS_PAY,
   headers: { "Content-Type": "application/json" },
 });
