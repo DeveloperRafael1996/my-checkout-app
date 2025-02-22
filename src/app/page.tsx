08d38624a3a149d62f968e8a71c840f016cdebc6
Rafael Guevara Aller <djzm80@gmail.com>
2025-02-21 23:39:39 -0500

    feat: success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home({
   });
 
   return (
-    <div className="bg-primary">
+    <div>
       <CheckoutFormNiubiz bodyPay={bodyPay} sessionKey={sessionKey} />
     </div>
   );
